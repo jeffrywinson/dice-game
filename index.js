8be5a7d88ba1bb14519f5e5e375f36ddf99e621e
kyoyaXD <133905665+kyoyaXD@users.noreply.github.com>
2024-06-12 22:04:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var rand1 = Math.floor(Math.random() * 6) + 1;
+var rand2 = Math.floor(Math.random() * 6) + 1;
+var randD1 = "./images/dice" + rand1 + ".png";
+var randD2 = "./images/dice" + rand2 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randD1);
+document.querySelectorAll("img")[1].setAttribute("src", randD2);
+if(rand1 > rand2){
+    document.querySelector("h1").textContent="🏅Player 1 Wins";
+}
+else if(rand1 === rand2){
+    document.querySelector("h1").textContent="🟰Tie🟰";
+}
+else{
+    document.querySelector("h1").textContent="Player 2 Wins🏅";
+}
